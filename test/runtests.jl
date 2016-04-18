d37c475ff59117a23de398401ae6da6ee99ec234
Samuel B. Johnson <sabjohnso@yahoo.com>
2016-04-17 21:01:42 -0500

    Coded

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,14 @@
 using PortNumbers
-using Base.Test
+using FactCheck
 
-# write your own tests here
-@test 1 == 1
+
+facts( "Port Numbers" ) do
+    p = PortNumber{UserPort}( 1234 )
+    @fact Int( p ) --> 1234
+    @fact string( p ) --> "1234"
+    @fact ASCIIString( p ) --> "1234"
+    @fact typeof( make_port_number( 22 )) --> PortNumber{EstablishedPort}
+    @fact typeof( make_port_number( 512 )) --> PortNumber{RootPort}
+    @fact typeof( make_port_number( 1234 )) --> PortNumber{UserPort}
+    
+end
